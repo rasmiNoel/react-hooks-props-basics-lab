66c12df6cf6aa13cd173e71203bb9ec265645896
rasmi <rasminoel@gmail.com>
2023-10-27 07:14:13 +0300

    Done With Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,9 @@ function About() {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* add your <Links /> component here */
+      <a href="linkedin.com/in/ryan-lee-1a1b1b1b1">Linkedin</a>
+       }
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar user={user}/>
+      <Home user={user} />
+      <About user={user}/>
     </div>
   );
 }
